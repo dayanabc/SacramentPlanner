1f5bb1afbcc0578fc2ccde6601b50d27a1d76b25
upload <uploaded@git.edu>
2018-12-14 09:30:26 -0700

    ..

---
+++ SacramentPlanner/SacramentPlanner/Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SacramentPlanner.Models;
+
+namespace SacramentPlanner.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SacramentPlanner.Models.SchoolContext _context;
+
+        public IndexModel(SacramentPlanner.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
