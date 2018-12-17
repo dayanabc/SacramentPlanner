b53cbb9fb703175ea7c0a666f90ee34768f48a1c
upload <uploaded@git.edu>
2018-12-17 14:47:10 -0700

    ...

--- SacramentPlanner/SacramentPlanner/Pages/Index.cshtml.cs
+++ SacramentPlanner/SacramentPlanner/Pages/Index.cshtml.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SacramentPlanner.Models;
 
 namespace SacramentPlanner.Pages
 {
     public class IndexModel : PageModel
     {
+        public IndexModel(SacramentPlanner.Models.SchoolContext context)
+        {
+            //_context = context;
+        }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+        public PaginatedList<Student> Student { get; set; }
+        public IList<Department> Department { get; set; }
+
         public void OnGet()
         {
 
